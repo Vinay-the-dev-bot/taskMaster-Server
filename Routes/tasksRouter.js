d7ac8a144d270363a9c6b9ad093e4a7f700ef025
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-24 15:55:48 +0530

    task router and userrouter

---
+++ Routes/tasksRouter.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { auth } = require("../MiddleWare/auth.Middleware");
+const { tasksModel } = require("../Model/tasksModel");
+const tasksRouter = express.Router();
+const dotenv = require("dotenv").config();
+// Adding a Task
+tasksRouter.post("/add", auth, async (req, res) => {
+  const task = new tasksModel({ ...req.body });
+  const tsk = await task.save({ new: true });
+  res.send({ msg: "TASK ADDED", tsk });
+});
+
+// Reading a Task
+tasksRouter.get("/", auth, async (req, res) => {
+  const task = await tasksModel.find({ userId: req.body.userId });
+  res.send(task);
+});
+
+tasksRouter.patch("/complete/:taskId", auth, async (req, res) => {
+  const task = await tasksModel.findByIdAndUpdate(
+    { _id: req.params.taskId },
+    { completed: true }
+  );
+  res.status(201).send({ msg: "Task Completed" });
+});
+tasksRouter.get("/:taskId", auth, async (req, res) => {
+  const task = await tasksModel.findOne({ _id: req.params.taskId });
+  res.send({ task });
+});
+
+// Editing a Task
+tasksRouter.patch("/:taskId", auth, async (req, res) => {
+  const task = await tasksModel.findOne({ _id: req.params.taskId });
+  if (task.userId == req.body.userId) {
+    await tasksModel.findByIdAndUpdate({ _id: req.params.taskId }, req.body);
+    res.send({ msg: "Task Edited" });
+  } else {
+    res.send({ msg: "NOT AUTHORIZED" });
+  }
+});
+
+// Deleting a Task
+tasksRouter.delete("/:taskId", auth, async (req, res) => {
+  const task = await tasksModel.findOne({ _id: req.params.taskId });
+  if (task) {
+    if (task.userId == req.body.userId) {
+      await tasksModel.findByIdAndDelete({ _id: req.params.taskId });
+      res.send({ msg: "Task Deleted" });
+    } else {
+      res.send({ msg: "NOT AUTHORIZED" });
+    }
+  } else {
+    res.send({ msg: "Task NOT FOUND" });
+  }
+});
+
+module.exports = { tasksRouter };
