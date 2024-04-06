e1fedfa5abc8a114ed922ca79266c21f9149cd78
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-04-06 20:11:06 +0530

    model update

--- Model/tasksModel.js
+++ Model/tasksModel.js
@@ -4,6 +4,10 @@ const tasksSchema = mongoose.Schema(
   {
     title: { type: String, require: true },
     description: { type: String, require: true },
+    subTasks: {
+      type: [{ title: String, completed: { type: Boolean, default: false } }],
+      default: [],
+    },
     userId: { type: String, require: true },
     author: { type: String, require: true },
     completed: { type: Boolean, default: false },
